2c8203c45f6c3da7f4bf6e80674c5dc506f9b1da
Swapna Jandhyala <jandhyalas@google.com>
2021-12-27 16:15:39 -0800

    adding files

--- release_tools/code_freeze.py
+++ release_tools/code_freeze.py
@@ -1,7 +1,8 @@
 # This script will do a code freeze by:
-# 1. Updating the 'pist' with the next release
-# 2. Creating a 'git branch' of the new SLKReleaseName
-# 3.
+# Create a 'branch' for the 'current' release thats found in 'release.plist'
+# Generate feature flag report with 'current' and 'previous'
+# Generate 'FF' report which shows the diff between new and old release
+# After 'branch' is created, change the 'verison' to next version in the plist file
 
 import os
 import plistlib
@@ -9,31 +10,9 @@ import csv
 
 def main():
 
-   fileName=os.path.expanduser('release.plist')
-
-   if os.path.exists(fileName):
-
-      pl=plistlib.readPlist(fileName)
-
-      print '\n The plist full contents is %s\n' % pl
-
-     # Increment 'version' to the next release
-
-      if 'SLKReleaseName' and 'CFBundleShortVersionString' in pl:
-         print 'Current release name is %s\n' % pl['SLKReleaseName']
-         print 'Current release version is %s\n' % pl['CFBundleShortVersionString']
-      else:
-         print 'There is no release name in the plist\n'
-
-   else:
-      print '%s does not exist, so can\'t be read' % fileName
-
-   with open('releng/release_info.csv', 'r') as file:
-       reader = csv.reader(file)
-       for row in reader:
-           if row[0] == "Cake":
-              next_release = row[0]+1
-
+   os.system("git checkout main")
+   #os.system("git status")
+   #os.system("git remote -v")
 
 
 
