2da5959208d1eb44efabe7651060aee76ebb50b9
Swapna Jandhyala <jan.swapna@gmail.com>
2021-12-29 15:29:19 -0800

    Creating new branch

--- release_tools/code_freeze.py
+++ release_tools/code_freeze.py
@@ -1,11 +1,21 @@
 # This script will do a code freeze by:
 # Create a 'branch' for the 'current' release that the user enters
-# Generate feature flag report with 'current' and 'previous' branch
+# Generate feature flag report between 'current' and 'previous' branch
 # Update the 'plist' file to next version
 
 import os
 import plistlib
 import csv
+
+def create_branch(rel_name,rel_number):
+    os.system("git checkout main")
+    os.system("git checkout -b %s/%s" % (rel_name,rel_number))
+    os.system("git add *")
+    os.system("git commit -m 'Creating new branch'")
+    os.system("git remote remove origin")
+    os.system("git remote add origin https://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@github.com/sj1984-23/Test.git")
+    os.system("git push --set-upstream %s/%s" % (rel_name,rel_number))
+    print("Created a new branch for %s/%s" %(rel_name,rel_number))
 
 def main():
 
@@ -14,20 +24,14 @@ def main():
  # Create a branch with current release for code freeze
    rel_name=raw_input("Enter the release name \n")
    rel_number=raw_input("Enter the release number \n")
-   os.system("git checkout -b %s/%s" % (rel_name,rel_number))
-   os.system("git add *")
-   os.system("git commit -m 'Creating new branch'")
-   os.system("git remote remove origin")
-   os.system("git remote add origin https://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@github.com/sj1984-23/Test.git")
-   os.system("git push --set-upstream %s/%s" % (rel_name,rel_number))
-   print("Created a new branch for %s/%s" %(rel_name,rel_number))
+   create_branch(rel_name,rel_number)
 
    # Find the previous release
    # Search for this release in 'release.csv', one entry
    # before is the previous release
    with open('../releng/release_info.csv', 'r') as file:
     reader = csv.reader(file)
-    #print(array.index('Beer'))
+
     for row in reader:
         items = (row[0] + "/" + row[1])
         array1.append(items)
@@ -50,7 +54,7 @@ def main():
     fileName=os.path.expanduser('../release.plist')
     if os.path.exists(fileName):
        pl=plistlib.readPlist(fileName)
-       
+
        # Update with new release name and version
        pl['SLKReleaseName']=rel_name
        plistlib.writePlist(pl, fileName)
@@ -64,3 +68,6 @@ def main():
           print 'There is no release name in the plist\n'
     else:
        print '%s does not exist, so can\'t be read' % fileName
+
+if __name__ == '__main__':
+   main()
